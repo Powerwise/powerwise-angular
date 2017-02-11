2219f68435f207dd6d27eb11712b90b8e24a33d5
Derek Munneke <dmunneke@the-lorax.lan>
2017-02-12 08:26:48 +1030

    fix lint error

--- src/client/app/app.module.ts
+++ src/client/app/app.module.ts
@@ -1,13 +1,13 @@
-import {APP_BASE_HREF} from '@angular/common';
-import {NgModule} from '@angular/core';
-import {HttpModule} from '@angular/http';
-import {BrowserModule} from '@angular/platform-browser';
+import { APP_BASE_HREF } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { HttpModule } from '@angular/http';
+import { BrowserModule } from '@angular/platform-browser';
 
-import {AboutModule} from './about/about.module';
-import {AppRoutingModule} from './app-routing.module';
-import {AppComponent} from './app.component';
-import {HomeModule} from './home/home.module';
-import {SharedModule} from './shared/shared.module';
+import { AboutModule } from './about/about.module';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeModule } from './home/home.module';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   imports: [
--- src/client/app/shared/shared.module.ts
+++ src/client/app/shared/shared.module.ts
@@ -1,12 +1,12 @@
-import {CommonModule} from '@angular/common';
-import {ModuleWithProviders, NgModule} from '@angular/core';
-import {FormsModule} from '@angular/forms';
-import {MaterialModule} from '@angular/material';
-import {RouterModule} from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { ModuleWithProviders, NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { MaterialModule } from '@angular/material';
+import { RouterModule } from '@angular/router';
 
-import {NameListService} from './name-list/name-list.service';
-import {NavbarComponent} from './navbar/navbar.component';
-import {ToolbarComponent} from './toolbar/toolbar.component';
+import { NameListService } from './name-list/name-list.service';
+import { NavbarComponent } from './navbar/navbar.component';
+import { ToolbarComponent } from './toolbar/toolbar.component';
 
 /**
  * Do not specify providers for modules that might be imported by a lazy loaded
