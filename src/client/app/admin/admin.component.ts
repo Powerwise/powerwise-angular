27206e7349069f32c1f161397aa4129c9abfb3ae
Paul Robinson <probinson@mackboopaulwork.unisa.edu.au>
2017-02-12 13:37:21 +1030

    fix

--- src/client/app/admin/admin.component.ts
+++ src/client/app/admin/admin.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {Store} from '@ngrx/store';
 import {Observable} from 'rxjs/Rx';
 
@@ -14,17 +14,21 @@ import * as fromRoot from '../shared/reducers/index';
   templateUrl: 'admin.component.html',
   styleUrls: ['admin.component.css']
 })
-export class AdminComponent implements OnInit {
+export class AdminComponent implements OnInit, OnDestroy {
   loading: Observable<boolean>;
   event: Observable<any>;
   devices: Observable<any>;
   total: Observable<any>;
+  interval: any;
   constructor(private store: Store<fromRoot.State>) {}
   ngOnInit() {
     this.loading = this.store.select(fromRoot.getSheddingLoading);
     this.event = this.store.select(fromRoot.getSheddingEvent);
     this.devices = this.store.select(fromRoot.getSheddingDevices);
     this.total = this.store.select(fromRoot.totalKillowats);
+    this.interval = setInterval(() => {
+      this.getDevices();
+    }, 2000);
   }
   getDevices() {
     this.store.dispatch(new shedding.GetDevicesAction());
@@ -32,5 +36,8 @@ export class AdminComponent implements OnInit {
   createEvent() {
     let payload = {timestamp: Date.now(), duration: 60 * 25};
     this.store.dispatch(new shedding.CreateAction(payload));
+  }
+  ngOnDestroy() {
+    clearInterval(this.interval);
   }
 }
